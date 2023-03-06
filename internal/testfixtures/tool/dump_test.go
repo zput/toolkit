87c1e5ecb8e062e125cea56be6b11a69b24fdb96
echo <echo@example.com>
2023-03-06 21:10:52 +0800

    add dump

---
+++ internal/testfixtures/tool/dump_test.go
@@ -0,0 +1,52 @@
+package tool
+
+import (
+	"database/sql"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func TestGenFixtureByExistDB(t *testing.T) {
+	sqlDb, err := NewSqlDB()
+	if err != nil {
+		panic(err)
+	}
+	type args struct {
+		db         *sql.DB
+		dialect    string
+		targetPath string
+		tables     []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "success",
+			args: args{
+				db:         sqlDb,
+				dialect:    "sqlite",
+				targetPath: "./archive",
+				tables:     nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GenFixtureByExistDB(tt.args.db, tt.args.dialect, tt.args.targetPath, tt.args.tables...); (err != nil) != tt.wantErr {
+				t.Errorf("GenFixtureByExistDB() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func NewSqlDB() (db *sql.DB, err error) {
+	var g *gorm.DB
+	g, err = gorm.Open(sqlite.Open("gorm.db"))
+	if err != nil {
+		return
+	}
+	db, err = g.DB()
+	return
+}
