36adb63d1f0cc828230beac22c5987b76f433939
timzhao <timzhao@futunn.com>
2024-04-13 14:35:00 +0800

    chore:修改断言结构

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,54 +1,80 @@
 package assert
 
 import (
-	"database/sql"
 	"fmt"
-	"github.com/smartystreets/goconvey/convey"
 	"reflect"
-	"time"
 )
 
-// compareTime 比较time
-func compareTime(v1, v2 interface{}) (eq bool) {
-	if value1, ok := v1.(time.Time); ok {
-		value2, _ := v2.(time.Time)
-		return value1.Equal(value2)
+func Equal(cur, exp interface{}) bool {
+	var isEqual = reflect.DeepEqual(cur, exp)
+	if isEqual {
+		return true
 	}
-	if value1, ok := v1.(sql.NullTime); ok {
-		value2, _ := v2.(sql.NullTime)
-		return value1.Valid == value2.Valid && value1.Time.Equal(value2.Time)
-	}
-	return
+	return equal(cur, exp)
 }
 
-func equal(field string, data1, data2 reflect.Value) (ret bool) {
-	// 0值存在一些特殊性，可能类型不一样
-	if data1.IsZero() && data2.IsZero() {
-		return true
-	}
-	if data1.Type() != data2.Type() {
-		_, _ = convey.Println("断言失败，类型不一致", field, data1.Type(), data2.Type())
+func equal(cur, exp interface{}) bool {
+	v1 := reflect.ValueOf(cur)
+	v2 := reflect.ValueOf(exp)
+	if v1.Type() != v2.Type() {
 		return false
 	}
-	//比较int类型
-	if compareInt(data1, data2) {
+	if v1.Kind() == reflect.Ptr {
+		v1 = v1.Elem()
+		v2 = v2.Elem()
+	}
+
+	// 提前判断自定义类型
+	if isSelfType(v1.Type()) {
+		return compareSelfType(v1.Type().Name(), v1, v2)
+	}
+
+	switch v2.Kind() {
+	case reflect.Slice:
+		if v1.Len() != v2.Len() {
+			fmt.Println("断言失败，字段长度不一致", v1.Len(), v2.Len())
+			return false
+		}
+		for j := 0; j < v2.Len(); j++ {
+			if !Equal(v1.Index(j).Interface(), v2.Index(j).Interface()) {
+				return false
+			}
+		}
 		return true
+	case reflect.Struct:
+		return Struct(v1.Interface(), v2.Interface())
 	}
-	tp := data1.Type().Name()
-	value1, value2 := data1.Interface(), data2.Interface()
-	switch tp {
-	case decimalType, nullDecimalType:
-		ret = compareDecimal(value1, value2)
-	case timeType, nullTimeType:
-		ret = compareTime(value1, value2)
-	default:
-		ret = reflect.DeepEqual(value1, value2)
+
+	return reflect.DeepEqual(v1.Interface(), v2.Interface())
+}
+
+func Struct(cur, exp interface{}) bool {
+	v1 := reflect.ValueOf(cur)
+	v2 := reflect.ValueOf(exp)
+	if v1.Type() != v2.Type() || v2.Kind() != reflect.Struct {
+		return false
 	}
-	if !ret {
-		msg := fmt.Sprintf("断言失败，字段：%s, 实际：%v，期望：%v", field, data1, data2)
-		_, _ = convey.Println(msg)
+
+	fmt.Printf("compare [%s] type, num field：%d\n", v2.Type().String(), v2.Type().NumField())
+
+	for i := 0; i < v2.NumField(); i++ {
+		fieldName := v2.Type().Field(i).Name
+		fmt.Printf("\t %d: %s\n", i, fieldName)
+
+		// 1.判断v1的字段值
+		vByV1I := v1.Field(i)
+		// 2.判断v2的字段是否为零值
+		vByV2I := v2.Field(i)
+		isZero, theVal := getTheRealVal(vByV2I)
+		if isZero {
+			continue
+		}
+		// 3.获取v2字段名
+		if !equal(vByV1I.Interface(), theVal.Interface()) {
+			return false
+		}
 	}
-	return
+	return true
 }
 
 func getTheRealVal(val reflect.Value) (isZero bool, ret reflect.Value) {
@@ -56,25 +82,8 @@ func getTheRealVal(val reflect.Value) (isZero bool, ret reflect.Value) {
 		isZero = true
 		return
 	}
+	if val.CanInterface() {
+		ret = val
+	}
 	return
-}
-
-func Assert(cur, exp interface{}) {
-	data1Val := reflect.ValueOf(cur)
-	data2Val := reflect.ValueOf(exp)
-
-	if data1Val.Type() != data2Val.Type() {
-		return
-	}
-
-	for i := 0; i < data1Val.NumField(); i++ {
-		field := data2Val.Type().Field(i).Name
-		data2Field := data2Val.Field(i)
-		isZero, theVal := getTheRealVal(data2Field)
-		if isZero {
-			continue
-		}
-		data1Field := data1Val.Field(i)
-		convey.So(equal(field, data1Field, theVal), convey.ShouldBeTrue)
-	}
 }
